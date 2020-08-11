833e7997fd01312650f0c64b8dfa8d35f9f69f62
Kent leung <snail@hotmail.com>
2020-08-10 22:41:48 -0700

    done

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { EmptyState, Layout, Page } from '@shopify/polaris';
 import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
+import store from 'store-js';
+import ResourceListWithProducts from '../components/ResourceList';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
@@ -8,6 +10,9 @@ class Index extends React.Component {
     state = { open: false };
 
     render() {
+
+        const emptyState = !store.get('ids');
+
         return (
             <Page>
                 <TitleBar
@@ -24,18 +29,22 @@ class Index extends React.Component {
                     onSelection={(resources) => this.handleSelection(resources)}
                     onCancel={() => this.setState({ open: false })}
                 />
-                <Layout>
-                    <EmptyState
-                        heading="Discount your products temporarily"
-                            action={{
-                                content: 'Select products',
-                                onAction: () => this.setState({ open: true }),
-                            }}
-                            image={img}
-                        >
-                        <p>Select products to change their price temporarily.</p>
-                    </EmptyState>
-                </Layout>
+                {emptyState ? (
+                    <Layout>
+                        <EmptyState
+                            heading="Discount your products temporarily"
+                                action={{
+                                    content: 'Select products',
+                                    onAction: () => this.setState({ open: true }),
+                                }}
+                                image={img}
+                            >
+                            <p>Select products to change their price temporarily.</p>
+                        </EmptyState>
+                    </Layout>
+                ) : (
+                    <ResourceListWithProducts />
+                )}
             </Page>
         )
     }
@@ -43,7 +52,9 @@ class Index extends React.Component {
     handleSelection = (resources) => {
         const idsFromResources = resources.selection.map((product) => product.id);
         this.setState({ open: false })
-        console.log(idsFromResources)
+
+        //This works well for testing your development app. for production app, better store it in database
+        store.set('ids', idsFromResources);
     };
 };
   
--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const session = require('koa-session');
 
 dotenv.config();
 const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
+
+//for Webhook
+const Router = require('koa-router');
+const {receiveWebhook, registerWebhook} = require('@shopify/koa-shopify-webhooks');
+
 const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
+const getSubscriptionUrl = require('./server/getSubscriptionUrl');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -17,12 +23,12 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
+const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY, HOST } = process.env;
 
 
 app.prepare().then(() => {
     const server = new Koa();
-
+    const router = new Router();
     server.use(session({ secure: true, sameSite: 'none' }, server));
     server.keys = [SHOPIFY_API_SECRET_KEY];
 
@@ -34,7 +40,7 @@ app.prepare().then(() => {
             scopes: ['read_products', 'write_products'],
 
             //afterAuth can use your own logic or redirect
-            afterAuth(ctx) {
+            async afterAuth(ctx) {
                 const { shop, accessToken } = ctx.session;
                 
                 //to set cookies to use "sameSite" and "secure" for the app to load in Chrome.
@@ -44,26 +50,46 @@ app.prepare().then(() => {
                     sameSite: 'none'
                 });
 
-                ctx.redirect('/');
+                //for webhook register
+                const registration = await registerWebhook({
+                    address: `${HOST}/webhooks/products/create`,
+                    topic: 'PRODUCTS_CREATE',
+                    accessToken,
+                    shop,
+                    apiVersion: ApiVersion.October19
+                });
+                if (registration.success) {
+                        console.log('Successfully registered webhook!');
+                    } else {
+                        console.log('Failed to register webhook', registration.result);
+                }
+
+                //for subscription plan
+                await getSubscriptionUrl(ctx, accessToken, shop);
             },
         }),
     );
     
+    //for webhook subscription
+    const webhook = receiveWebhook({secret: SHOPIFY_API_SECRET_KEY});
+    router.post('/webhooks/products/create', webhook, (ctx) => {
+        console.log('received webhook: ', ctx.state.webhook);
+    });
+    
     //Shopify koa middleware to securely proxy graphQL requests from Shopify, and api version.
     server.use(graphQLProxy({version: ApiVersion.October19}))
     //The verifyRequest redirects users to the OAuth route if they haven’t been authenticated.
-    server.use(verifyRequest());
-
-    server.use(async (ctx) => {
+    router.all('/(.*)', verifyRequest(), async (ctx) => {
         await handle(ctx.req, ctx.res);
         ctx.respond = false;
         ctx.res.statusCode = 200;
-        return
     });
+    server.use(router.allowedMethods());
+    server.use(router.routes());
 
     server.listen(port, () => {
         console.log(`> Ready on http://localhost:${port}`);
-      });
+    });
 });
 
 
